04d9c64e83f3a6588737d624353775e6267b6325
CodeNinjas9 <codeninjas.nwaustin9@gmail.com>
2025-04-19 13:57:00 -0500

    Hi

--- My project/Assets/Emitter.cs
+++ My project/Assets/Emitter.cs
@@ -1,40 +1,77 @@
 
-public class Emitter: MonoBehaviour{
+public class RayTracedAudio: MonoBehaviour
+{
     public int maxBounces = 9;
-    public int poolSize;
-    public int audioZone;
+    public int numRays;
     public int layerMask = (1 << 2) | (1 << 1);
     public GameObject player;
-    public float soundCalculation; 
+    public float occlusion; 
+    public Dictionary<int, float[]> audioData;
+    public List<float[]> bufferedData;
     void Start() 
     {
         
     }
     void FixedUpdate()
     {
-        Vector3 previousVector = Vector3.forward;
-        for(int i = 0; i <= poolSize; i++)
+        for(int i = 0; i <= numRays; i++)
         {
-            ray = new Ray(transform.position, previousVector);
-            RayCastHit hit;
-            RayCast cast = Physics.RayCast(ray.origin, ray.direction, out hit, Mathf.Infinity, layerMask);
-            int bounces = 1; 
-            while(bounces <= maxBounces++)
+            Vector3 ray = Vector3.forward * Quaternion.Euler(0, (360/numRays) * i, 0);
+            if(Physics.Raycast(ray, transform.TransformDirection(Vector3.forward), out hit, Vector3.Distance(ray, player.transform.position), layerMask))
             {
-                if(cast)
+                occlusion += 1/numRays;
+            }
+        }
+    }
+    void ResampleDelayLines(int maxiter, int iterations, List<float[]> bufferedData) 
+    { 
+        int random = Random.Range(0, bufferedData.Length);
+        bufferedData.Add(audioData[random]);
+        if(iterations >= maxiter)
+        {
+            return bufferedData;
+        }
+        ResampleDelayLines(maxiter, iterations++, bufferedData);
+    }
+    void FeedbackDelayNetwork(int iterations, int maxiter, List<float[]> delayLines, int rows, int cols) 
+    {
+        if(iterations >= maxiter)
+        {
+            return delayLines;
+        }
+        Matrix4x4 randomMatrix = GenerateMatrix();
+        for(int z = 0; z < delayLines.Length; z++)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
                 {
-                    length = Vector3.Distance(ray.origin, hit.point);
-                    ray = new Ray(transform.position, Vector3.reflect(ray.direction, hit.normal));
-                    bounces++;
-                    cast = Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, layerMask);
-                }
-                else
-                {
-                    break;
+                    delayLines[z][i + 1 * j + 1] *= randomMatrix[i][j];
                 }
             }
-            previousVector = previousVector * Quaternion.Euler(0, 360/poolSize, 0);
         }
-        soundCalculation = (soundCalculation / Vector.Distance(transform.position, player.transform.position) * bounces) ** 1/audioZone; 
+        foreach(var item in delayLines)
+        {
+            for(int z = 0; z <= delayLines.Length; z++)
+            {
+                for(int i = 0; i <= item.Length; i++)
+                {
+                    delayLines[z][i] *= item[i];
+                }
+            }
+        }
+        FeedbackDelayNetwork(iterations++, maxiter, delayLines);
+    }
+    public void GenerateMatrix()
+    {
+        Matrix4x4 randomMatrix = new Matrix4x4();
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                randomMatrix[i, j] = Random.Range(minRange, maxRange);
+            }
+        }
+        return randomMatrix; 
     }
 }
